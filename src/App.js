46997b87f441d984350696b935e4399b8aca5ce7
Ori.Michaeli <Ori.Michaeli6@gmail.com>
2020-02-19 10:59:29 +0200

    testing feature branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           To edit -  <code>src/App.js</code> and save to reload.
+          Ori
         </p>
         <a
           className="App-link"
